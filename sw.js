9b967e9428fa3969b80c3c31142aede61bfe3955
yuriveron12-hue <yuriveron12@gmail.com>
2026-02-09 18:05:05 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,16 @@
-const CACHE = "relatorio-ed31-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./logo.png"];
+const CACHE = "relatorio-ed31-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js", "./logo.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE) ? caches.delete(k) : Promise.resolve())))
   );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
