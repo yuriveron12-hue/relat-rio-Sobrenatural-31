5c52977654d375e126f79a7bebdbc377ef1425a7
yuriveron12-hue <yuriveron12@gmail.com>
2026-02-09 17:04:16 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "relatorio-ed31-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./logo.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
